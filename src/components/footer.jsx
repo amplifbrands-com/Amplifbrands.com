7966d789225ce85dad37a1b94db276f2a7b55ed8
Vimal Bhatt <157715816+DevHydeOut@users.noreply.github.com>
2024-02-16 20:23:23 +0530

    navbar added with mobile navigation

---
+++ src/components/footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import "../styles/footer.css"
+const Footer = () => {
+  return (
+   <div>
+   
+   </div>
+  );
+};
+
+export default Footer;
