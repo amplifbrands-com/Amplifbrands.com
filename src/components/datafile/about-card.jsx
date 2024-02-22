d0b683e6be805525bacc8e90bb652586820a98c7
GopiRanganathan <gopiranga9@gmail.com>
2024-02-22 13:21:38 +0530

    change in about-data.js

--- src/components/datafile/about-card.jsx
+++ src/components/datafile/about-card.jsx
@@ -6,9 +6,9 @@ function Card({image, name, position, about}){
             <div className='about-expert-card-image-outer'>
                 <img src={image} alt={`${name} image`} width={100} height={100} className="about-expert-card-image"/>
             </div>
-            <div className="about-expert-card-heading">
+            <div className="about-expert-card-heading mb-4">
                 <h4 className="about-expert-card-h4">{name}</h4>
-                <span className="about-expert-card-span">{position}</span>
+                <span className="about-expert-card-span" >{position}</span>
             </div>
             <div className="about-expert-card-para">
                 <p className="about-expert-card-p">{about}</p>
