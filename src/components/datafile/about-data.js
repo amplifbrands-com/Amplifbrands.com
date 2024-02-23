187dee4adbc483bb8d2380403632c2f9c49e6813
GopiRanganathan <gopiranga9@gmail.com>
2024-02-24 00:20:48 +0530

    our-experts data added

--- src/components/datafile/about-data.js
+++ src/components/datafile/about-data.js
@@ -1,5 +1,15 @@
 import femalePlaceholder from "/amplif-members/female-placeholder.jpg"
-
+import AnselmSadiq from "/amplif-members/Anselm-Sadiq.jpg"
+import DavidZakari from "/amplif-members/David-Zakari.jpg"
+import EmmanuelAberuagba from "/amplif-members/Emmanuel-Aberuagba.jpg"
+import HammadNazir from "/amplif-members/Hammad-Nazir.png"
+import KedarKulkarni from "/amplif-members/Kedar-Kulkarni.png"
+import NabanitaGhosh from "/amplif-members/Nabanita-Ghosh.jpg"
+import Naveenkumar from "/amplif-members/Naveenkumar-V.jpg"
+import SenthilKumar from "/amplif-members/Senthil-Kumar.jpg"
+import Victoria from "/amplif-members/Victoria-Oremeyi.jpeg"
+import VinithRay from "/amplif-members/Vinith-Ray.jpg"
+import AniketRay from "/amplif-members/Aniket-Ray.jpeg"
 import Expert from "../../assets/about-us/about-expert.svg"
 
 // Save image file in /public/amplif-members/your-image
@@ -12,14 +22,14 @@ export const data = [
         image:Expert,
         name:'Vimal',
         position:"Don't Know Yet",
-        about:"Lorem ipsum dolor sit amet consectetur adipisicing elit."
+        about:""
     },
     {   
         id: 2,
-        image:Expert,
-        name:'Vimal',
-        position:"Don't Know Yet",
-        about:"Lorem ipsum dolor sit amet consectetur adipisicing elit."
+        image:AnselmSadiq,
+        name:'Anselm Sadiq',
+        position:"Content writer",
+        about:""
     },
     {   
         id: 3,
@@ -30,37 +40,72 @@ export const data = [
     },
     {   
         id: 4,
-        image:Expert,
-        name:'Vimal',
-        position:"Don't Know Yet",
-        about:"Lorem ipsum dolor sit amet consectetur adipisicing elit."
+        image:DavidZakari,
+        name:'Zakari Adeiza David',
+        position:"Content writer",
+        about:""
     },
     {   
         id: 5,
-        image:Expert,
-        name:'Vimal',
-        position:"Don't Know Yet",
-        about:"Lorem ipsum dolor sit amet consectetur adipisicing elit."
+        image:SenthilKumar,
+        name:'Senthil Kumar',
+        position:"Wordpress Developer",
+        about:""
     },
     {   
         id: 6,
-        image:Expert,
-        name:'Vimal',
-        position:"Don't Know Yet",
-        about:"Lorem ipsum dolor sit amet consectetur adipisicing elit."
+        image:Victoria,
+        name:'Victoria',
+        position:"Administrative Assistant",
+        about:""
     },
     {   
         id: 7,
-        image:Expert,
-        name:'Vimal',
-        position:"Don't Know Yet",
-        about:"Lorem ipsum dolor sit amet consectetur adipisicing elit."
+        image:EmmanuelAberuagba,
+        name:'Emmanuel Aberuagba',
+        position:"Project manager | frontend developer",
+        about:""
     },
     {   
         id: 8,
-        image:Expert,
-        name:'Vimal',
-        position:"Don't Know Yet",
-        about:"Lorem ipsum dolor sit amet consectetur adipisicing elit."
+        image:Naveenkumar,
+        name:'Naveenkumar V',
+        position:"Front end developer",
+        about:""
+    },
+    {   
+        id: 9,
+        image:HammadNazir,
+        name:'Hammad Ahmad',
+        position:"Front end developer",
+        about:""
+    },
+    {   
+        id: 10,
+        image:KedarKulkarni,
+        name:'Kedar',
+        position:"UI/UX Designer",
+        about:""
+    },
+    {   
+        id: 11,
+        image:VinithRay,
+        name:'Vinith Ray',
+        position:"Front end developer",
+        about:""
+    },
+    {   
+        id: 12,
+        image:NabanitaGhosh,
+        name:'Nabanita Ghosh',
+        position:"Front end developer",
+        about:""
+    },
+    {   
+        id: 13,
+        image:AniketRay,
+        name:'Aniket Ray',
+        position:"Project Lead (Software Development)",
+        about:""
     },
 ]
